9721e5c11b4ac95282200ec833f2d469c0a90904
p.korolev <amlerok96@gmail.com>
2022-05-25 22:36:09 +0300

    add Displayname

--- src/test/java/tests/WikipediaTests.java
+++ src/test/java/tests/WikipediaTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +15,7 @@ import static io.qameta.allure.Allure.step;
 @Tag("selenide")
 public class WikipediaTests extends TestBase{
     @Test
+    @DisplayName("Тест на проверку четырех шагов адаптации")
     void onboardingStepsTest() {
 
         step("Убедитесь, что текст «Свободная энциклопедия… на более чем 300 языках» виден", () -> {
@@ -21,7 +23,7 @@ public class WikipediaTests extends TestBase{
                     .shouldHave(text("The Free Encyclopedia …in over 300 languages"));
         });
 
-        step("Click on 'Сontinue'", () ->
+        step("Нажмите «Продолжить»", () ->
                 $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
 
         step("Убедитесь, что текст «Новые способы исследования» виден", () -> {
@@ -47,6 +49,7 @@ public class WikipediaTests extends TestBase{
     }
 
     @Test
+    @DisplayName("Тест на проверку работы строки поиска")
     void searchTest() {
         step("Нажмите «Пропустить»", () -> {
             $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();
