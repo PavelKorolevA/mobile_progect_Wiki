f612fe400d0667fd2ad30db66d23cb70033df1ad
p.korolev <amlerok96@gmail.com>
2022-05-25 21:50:01 +0300

    Initial commit

---
+++ src/test/java/tests/WikipediaTests.java
@@ -0,0 +1,61 @@
+package tests;
+
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
+
+@Tag("selenide")
+public class WikipediaTests extends TestBase{
+    @Test
+    void onboardingStepsTest() {
+
+        step("Убедитесь, что текст «Свободная энциклопедия… на более чем 300 языках» виден", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("The Free Encyclopedia …in over 300 languages"));
+        });
+
+        step("Click on 'Сontinue'", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Убедитесь, что текст «Новые способы исследования» виден", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("New ways to explore"));
+        });
+
+        step("Нажмите «Продолжить»", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Убедитесь, что текст «Чтение списков с синхронизацией» виден", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Reading lists with sync"));
+        });
+
+        step("Нажмите «Продолжить»", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click());
+
+        step("Убедитесь, что текст «Отправить анонимные данные» виден", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/primaryTextView"))
+                    .shouldHave(text("Send anonymous data"));
+        });
+    }
+
+    @Test
+    void searchTest() {
+        step("Нажмите «Пропустить»", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click();
+        });
+        step("Введите в поиск", () -> {
+            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+        });
+        step("Проверить результат поиска", () ->
+                $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0)));
+    }
+}
